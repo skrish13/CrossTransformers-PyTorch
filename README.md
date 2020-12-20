59e4c6b22d68084d67e32fe783430f9457ec18bb
Sri Krishna <skrish1113@gmail.com>
2020-12-20 16:25:21 +0530

    added details

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # CrossTransformers-PyTorch
+
+Unofficial PyTorch implementation
+ - [CrossTransformers: spatially-aware few-shot transfer](http://arxiv.org/abs/2007.11498)
+    - Carl Doersch, Ankush Gupta, Andrew Zisserman
+    - NeurIPS 2020
+
+![CrossTransformers](architecture.jpg)
+
+## Main contributions
+
+- CrossTransformers Architecture
+- SimCLR episodes
+
+## Implementation details
+
+- Resnet34, output feature map 14x14 by using dilated conv
+- Higher image resolution (224x224)
+- Strong data augmentation following [2]
+
+## TODO
+
+- [x] CTX sanity check on miniImagenet
+- [ ] CTX on Meta-Dataset [1]
+- [ ] CTX + SimCLR Eps
+- [ ] CTX + SimCLR Eps + Aug
+
+## Acknowledgements
+
+- miniImagenet experiments based out of DN4 [codebase](https://github.com/WenbinLee/DN4/blob/master/dataset/datasets_csv.py).
+
+## References
+
+[1] [Meta-Dataset: A Dataset of Datasets for Learning to Learn from Few Examples.](https://arxiv.org/abs/1903.03096)
+
+[2] [Optimized generic feature learning for few-shot classification across domains.](https://arxiv.org/abs/2001.07926)
--- model.py
+++ model.py
@@ -1,64 +1,87 @@
 import torch
 import torch.nn as nn
+from resnet import resnet34
 
 class CrossAttention(nn.Module):
-    def __init__(self, config={}):
-        super().__init__()
+	def __init__(self, config={}):
+		super().__init__()
 
-        self.config = {
-            'query_feat_size': 512,
-            'support_feat_size': 512,
-            'key_head_dim': 128,
-            'value_head_dim': 128,
-        }
+		self.feature_extractor = resnet34()
 
-        D = self.config['query_feat_size']
-        Ds = self.config['support_feat_size']
-        dk = self.config['key_head_dim']
-        dv = self.config['value_head_dim']
+		self.config = {
+			'query_feat_size': 512,
+			'support_feat_size': 512,
+			'key_head_dim': 128,
+			'value_head_dim': 128,
+		}
 
-        self.key_head = nn.Conv2d(Ds, dk, 1, bias=False)
-        self.query_head = nn.Conv2d(D, dk, 1, bias=False)
-        self.value_head = nn.Conv2d(Ds, dv, 1, bias=False)
+		D = self.config['query_feat_size']
+		Ds = self.config['support_feat_size']
+		dk = self.config['key_head_dim']
+		dv = self.config['value_head_dim']
 
-        ## In the paper authors use key and query head to be same; End of Section 3.2
-        ## Feel free to comment if you dont prefer this
-        self.query_head = self.key_head
+		self.key_head = nn.Conv2d(Ds, dk, 1, bias=False)
+		self.query_head = nn.Conv2d(D, dk, 1, bias=False)
+		self.value_head = nn.Conv2d(Ds, dv, 1, bias=False)
 
-    def forward(self, query, support):
-        """ query   B x D x H x W
-            support Nc x Ds x Hs x Ws
-        """
+		## In the paper authors use key and query head to be same; End of Section 3.2
+		## Feel free to comment if you dont prefer this
+		self.query_head = self.key_head
 
-        query = self.query_head(query)
-        query_value = self.value_head(query)
-        support_key = self.key_head(support)
-        support_value = self.value_head(support)
+	def forward(self, query, support):
+		""" query   B x D x H x W
+			support Nc x Nk x Ds x Hs x Ws (#CLASSES x #SHOT x #DIMENSIONS)
+		"""
 
-        dk = query.shape[1]
+		Nc, Nk, Ds, Hs, Ws = support.shape
 
-        ## flatten pixels for simplicity
-        query = query.view(query.shape[0], query.shape[1], -1)
-        query_value = query_value.view(query_value.shape[0], query_value.shape[1], -1)
-        support_key = support_key.view(support_key.shape[0], support_key.shape[1], -1)
-        support_value = support_value.view(support_value.shape[0], support_value.shape[1], -1)
+		### Step 1: Get query and support features
+		query_image_features = self.feature_extractor(query)
+		support_image_features = self.feature_extractor(support.view(Nc*Nk, Ds, Hs, Ws))
 
-        ## dot product over every query and key features
-        attn_weights = torch.einsum('bdu,ndv->bnuv', query, key) / dk**.5
-        ## sum over support set length 'n' and all feature vectors 'v' in each support image
-        attn_weights = torch.nn.functional.softmax(attn_weights, dim=3)
-        attn_weights = torch.nn.functional.softmax(attn_weights, dim=1)
-        ## sum over support set length 'n' and all feature vectors 'v' in each support image
-        query_aligned = torch.einsum('bnuv,ndv->bdv', attn_weights, support_value)
 
-        ## reshape for cdist
-        query_aligned = query_aligned.permute(0,2,1)
-        query_value = query_value.permute(0,2,1)     
+		### Step 2: Calculate query aligned prototype
+		query = self.query_head(query_image_features)
+		support_key = self.key_head(support_image_features)
+		support_value = self.value_head(support_image_features)
 
-        ## calculate scalar distance
-        distance = torch.cdist(query_aligned, query_value, p=2)
-        distance = distance**2
-        B,P,R = distance.shape
-        distance = distance.sum(dim=(1,2)) / (P*R)
+		dk = query.shape[1]
 
-        return distance
+		## flatten pixels in query (p in the paper)
+		query = query.view(query.shape[0], query.shape[1], -1)
+		
+        ## flatten pixels & k-shot in support (j & m in the paper respectively)
+		support_key = support_key.view(Nc, Nk, support_key.shape[1], -1)
+		support_value = support_value.view(Nc, Nk, support_value.shape[1], -1)
+
+		support_key = support_key.permute(0, 2, 3, 1)		
+		support_value = support_value.permute(0, 2, 3, 1)
+
+		support_key = support_key.view(Nc, support_key.shape[1], -1)		
+		support_value = support_value.view(Nc, support_value.shape[1], -1)
+
+		## v is j images' m pixels, ie k-shot*h*w
+		attn_weights = torch.einsum('bdp,ndv->bnpv', query, support_key) * (dk ** -0.5)
+		attn_weights = torch.nn.functional.softmax(attn_weights, dim=-1)
+		
+        ## get weighted sum of support values
+		support_value = support_value.unsqueeze(0).expand(attn_weights.shape[0], -1, -1, -1)
+		query_aligned_prototype = torch.einsum('bnpv,bndv->bnpd', attn_weights, support_value)
+
+		### Step 3: Calculate query value
+		query_value = self.value_head(query_image_features)
+		query_value = query_value.view(query_value.shape[0], -1, query_value.shape[1]) ##bpd
+		
+		### Step 4: Calculate distance between queries and supports
+		distances = []
+		for classid in range(query_aligned_prototype.shape[1]):
+			dxc = torch.cdist(query_aligned_prototype[:, classid], 
+											query_value, p=2)
+			dxc = dxc**2
+			B,P,R = dxc.shape
+			dxc = dxc.sum(dim=(1,2)) / (P*R)
+			distances.append(dxc)
+		
+		distances = torch.stack(distances, dim=1)
+
+		return distances
